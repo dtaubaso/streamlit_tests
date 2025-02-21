4f0dda1f6826bacbe2f4a97a9e42e0598ac38e78
dtaubaso <d.taubaso@gmail.com>
2025-02-21 12:42:47 -0300

    agrega expander al video

--- app.py
+++ app.py
@@ -88,11 +88,12 @@ with st.expander("Ver argumentos"):
 
 video_url = st.text_input("Ingrese la url del video")
 
-if st.button("Ver video"):
-    if video_url:
-        try:
-            st.video(video_url)
-        except Exception as e:
-            st.error(f"Ocurrio un error {e}")
-    else:
-        st.error("No hay url de video")
+with st.expander("Video con intrucciones")
+    if st.button("Ver video"):
+        if video_url:
+            try:
+                st.video(video_url)
+            except Exception as e:
+                st.error(f"Ocurrio un error {e}")
+        else:
+            st.error("No hay url de video")
